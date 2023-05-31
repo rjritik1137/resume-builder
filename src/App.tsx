9911a52ad74c475a8929136c9ee9bbd28bd4d9fe
Ritik Jain <300073191@BLRM-300073191.local>
2023-05-31 17:46:08 +0530

    added eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import './styles.css'
-import React from 'react'
+import React, { useEffect } from 'react'
 export const App = ()=>{
+    const p = 1
+    
+    useEffect(()=>{
+        console.log(p)
+       
+    },[p])
     return (
         <React.Fragment>
     <h1>React typescript webpack starter - {process.env.NODE_ENV} {process.env.name}</h1>
