939695965d67baaa9ff5f0c9567a6b988fd84218
Ritik Jain <300073191@BLRM-300073191.local>
2023-05-31 17:54:10 +0530

    added prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './styles.css'
-import React, { useEffect } from 'react'
-export const App = ()=>{
-    const p = 1
-    
-    useEffect(()=>{
-        console.log(p)
-       
-    },[p])
-    return (
-        <React.Fragment>
-    <h1>React typescript webpack starter - {process.env.NODE_ENV} {process.env.name}</h1>
+import React from 'react'
+export const App = () => {
+  return (
+    <React.Fragment>
+      <h1>
+        React typescript webpack starter - {process.env.NODE_ENV}{' '}
+        {process.env.name}
+      </h1>
     </React.Fragment>
-    )
+  )
 }
