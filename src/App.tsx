f6c20441c3b8c928e12492d59f70e2d7a983328f
Ritik Jain <300073191@BLRM-300073191.local>
2023-05-31 16:12:47 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export const App = ()=>{
+    return <h1>React typescript webpack starter</h1>
+}
---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'), /* This is the entry point which points to index.tsx present inside the src folder */
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'], /**  This allows us to leave of the file extension when importing, webpack basically check .tsx first, if not found, then .ts, then .js*/
+  },
+  module: {
+    rules: [
+      // this rule is telling that webpack should use babel-loader of .ts, .js and .tsx file exclusding node_modules 
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader', 
+          },
+        ],
+      },
+    ],
+  },
+  /** Specification of output direactly for bundled code */
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+  },
+  mode:"development",
+  /** This HtmlWebpackPlugin helps to inject the bundled..js file into the index.html file, and place this html file into the build folder */
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+  ],
+}
